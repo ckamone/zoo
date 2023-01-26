17e64dee8c29a8ee11daa0a0a3dbd3cd8e9774c9
ckamone <ckamone@gmail.com>
2023-01-26 18:11:54 +0300

    fix lint

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,9 +1,20 @@
+"""
+info
+:return:
+"""
+
 import pytest
 from flask.testing import FlaskClient
+
 from ..app import app
+
 
 @pytest.fixture
 def client() -> FlaskClient:
+    """
+    info
+    :return:
+    """
     with app.test_client() as test_client:
         with app.app_context():
             yield test_client
--- testing/test_main.py
+++ testing/test_main.py
@@ -1,14 +1,33 @@
+"""
+info
+:return:
+"""
+
 def test_get_homepage(client):
+    """
+    info
+    :return:
+    """
     url = '/'
     response = client.get(url)
     assert response.status_code == 200
 
+
 def test_get_products(client):
+    """
+    info
+    :return:
+    """
     url = '/products/'
     response = client.get(url)
     assert response.status_code == 200
 
+
 def test_get_product(client):
+    """
+    info
+    :return:
+    """
     url = '/products/1/'
     response = client.get(url)
     assert response.status_code == 200
